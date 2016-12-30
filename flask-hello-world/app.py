e00621309793bbb394c6531675f7e40285a4acc0
Michael Lee <msciences_dev@outlook.com>
2016-12-30 15:10:28 -0500

    Added support for additional paths.

--- flask-hello-world/app.py
+++ flask-hello-world/app.py
@@ -6,6 +6,9 @@ from flask import Flask
 # create the application object
 app = Flask(__name__)
 
+# enable error handling
+app.config["DEBUG"] = True
+
 # use the decorator pattern to
 # link the view function to a url
 @app.route("/")
@@ -13,7 +16,35 @@ app = Flask(__name__)
 
 # define the view using a function, which returns a string
 def hello_world():
-	return "Hello, World!"
+	return "Hello, World 5-1!"
+
+# dynamic routes
+@app.route("/test/<search_query>")
+def search(search_query):
+  return search_query
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+  print(value + 1)
+  return "correct"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+  print(value + 1)
+  return "correct"
+
+# dynamic route that accepts slashes
+@app.route("/path/<path:value>")
+def path_type(value):
+  print(value)
+  return "correct"
+
+@app.route("/name/<name>")
+def index(name):
+  if name.lower() == "michael" :
+    return "Hello, {}".format(name), 200
+  else :
+    return "Not Found", 404
 
 # start the development server using the run() method
 if __name__ == "__main__":
