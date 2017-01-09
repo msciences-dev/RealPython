494f2bd62035fb6e67a9cd973262ef7b85ed2751
Michael Lee <msciences_dev@outlook.com>
2017-01-09 15:43:33 -0500

    Monday's weekly commit.

--- sql/02_sql.py
+++ sql/02_sql.py
@@ -4,18 +4,10 @@
 # import the sqlite3 library
 import sqlite3
 
-# create the connection object
-conn = sqlite3.connect("new.db")
+with sqlite3.connect("new.db") as connection:
+  c = connection.cursor()
 
-# get a cursor object used to execute SQL commands
-cursor = conn.cursor()
-
-# insert data
-cursor.execute("INSERT INTO population VALUES('New York City', \
-  'NY', 8400000)")
-cursor.execute("INSERT INTO population VALUES('San Francisco', \
-  'CA', 800000)")
-# commit the changes
-conn.commit()
-# close the database connection
-conn.close()
+  c.execute("INSERT INTO population VALUES('Chicago 3', \
+    'IL', 4400000)")
+  c.execute("INSERT INTO population VALUES('Boston 3', \
+    'MA', 800000)")
